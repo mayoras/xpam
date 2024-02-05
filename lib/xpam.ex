02cc61ab769e688e09b9d05dfbcd9b8713a1cf6c
Cesar Mayora <cesarmay@protonmail.com>
2024-02-05 14:57:12 +0100

    Initial commit

---
+++ lib/xpam.ex
@@ -0,0 +1,18 @@
+defmodule Xpam do
+  @moduledoc """
+  Documentation for `Xpam`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Xpam.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xpam_test.exs
@@ -0,0 +1,8 @@
+defmodule XpamTest do
+  use ExUnit.Case
+  doctest Xpam
+
+  test "greets the world" do
+    assert Xpam.hello() == :world
+  end
+end
